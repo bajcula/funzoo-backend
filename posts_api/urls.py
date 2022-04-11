95ec142f4bec9e8194c3b49dec66f953fe37dc5b
bajculinho <97913469+Bajculinho@users.noreply.github.com>
2022-04-10 19:41:19 -0500

    setup

--- posts_api/urls.py
+++ posts_api/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.PostList.as_view(), name='post_list'), # api/contacts will be routed to the ContactList view for handling
-    path('<int:pk>', views.PostDetail.as_view(), name='post_detail'), # api/contacts will be routed to the ContactDetail view for handling
+    path('<int:pk>/', views.PostDetail.as_view(), name='post_detail'), # api/contacts will be routed to the ContactDetail view for handling
 ]
