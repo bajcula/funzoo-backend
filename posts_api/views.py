2fbcaeff85f995c5e024f0cac8ffaaa7b7e33cdd
bajculinho <97913469+Bajculinho@users.noreply.github.com>
2022-04-15 19:59:07 -0500

    saved posts implemented

--- posts_api/views.py
+++ posts_api/views.py
@@ -33,6 +33,68 @@ class UsersPostsList(generics.ListCreateAPIView):
         data = list(filteredPosts.values())
         return JsonResponse(data, safe=False)   
         
+class UsersSavedPostsList(generics.ListCreateAPIView):
+    # queryset = Post.objects.all().order_by('id') # tell django how to retrieve all objects from the DB, order by id ascending
+    serializer_class = PostSerializer # tell django what serializer to use
+    http_method_names = ['get']
+
+    # 2nd TRY
+    def get(self, request, **kwargs):
+        # PostsSavedByUsers = Post.users_liked_by.through
+        # posts = Post.objects.all()
+        # filteredPosts = posts.filter(authorID_id=kwargs["id"])
+        # filteredPosts = posts.filter(authorID_id=kwargs["id"])
+        result = Post.objects.all()
+        # result.values('users_liked_by').filter(id=kwargs["id"])
+        usersSavedPostsTuples = result.values_list('users_liked_by', 'id')
+
+        usersSavedPostsTuples = list(usersSavedPostsTuples)
+        # for res in result.iterator():
+        #     print (res.id, res.users_liked_by.through)
+        # usersSavedPosts = PostsSavedByUsers.objects.filter(myuser_id=kwargs["id"]).values('myuser')
+        # print('posts saved by users', PostsSavedByUsers)
+        # print('this user saved posts', usersSavedPosts)
+        # posts_in_saved_table = Post.objects.filter(
+        #     id__in=usersSavedPosts
+        # )
+        usersRealList = []
+        data = []
+        print('list made', usersSavedPostsTuples)
+        for item in usersSavedPostsTuples:
+            print ('this is ids of users who liked it', item[0], 'and this is the posts id', item[1])
+            if item[0] == kwargs["id"]:
+                usersRealList.append(Post.objects.get(id=item[1]))
+                print('this is the pair', item[0], item[1])
+        
+        print('final list', usersRealList)
+  
+        def PostToDictionary(post):
+            if post == None:
+                return None
+
+            dictionary = {}
+            dictionary["id"] = post.id
+            dictionary["title"] = post.title
+            dictionary["pet_category"] = post.pet_category
+            dictionary["description"] = post.description
+            dictionary["location"] = post.location
+            dictionary["authorName"] = post.authorName
+            dictionary["created_at"] = post.created_at
+            dictionary["authorID_id"] = post.authorID_id
+            dictionary["img"] = str(post.img)
+            
+            return dictionary
+        
+        for list_item in usersRealList:
+            data.append(PostToDictionary(list_item))
+            
+        print('this is data', data)
+            
+        # data = list(posts_in_saved_table.values())
+        return JsonResponse(data, safe=False) 
+    
+
+
      
 def LikeView(request, postID, usersID):
     post = Post.objects.get(id=postID)
